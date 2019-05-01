6740a86315f19b8a0dc04874484998f26fab84cd
adrianlemess <adrianlemess@gmail.com>
2019-05-01 19:06:02 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get install -y make build-essential libicu48 libicu-dev
 apt-get install -y git
-apt-get install -y ruby1.9.1-full
+apt-get install -y ruby-full
 gem install gollum
